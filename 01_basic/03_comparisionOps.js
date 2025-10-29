d5748976375ccbb973c52b0c1701f19c324c626a
saipraneeth9396 <praneeth9396@gmail.com>
2025-10-29 10:49:37 +0000

    type conversion

---
+++ 01_basic/03_comparisionOps.js
@@ -0,0 +1,73 @@
+// //number
+// let num = 24
+
+// let newNum1 = String(num);
+// let newNum2 = Boolean(num);
+
+// console.log(newNum1);
+// console.log(typeof newNum1);
+// console.log(newNum2); //for neg also true but for 0 = false
+// console.log(typeof newNum2);
+
+
+// //string
+// let s = "33abc"
+
+// let newS1 = Number(s);
+// let newS2 = Boolean(s);
+
+// console.log(newS1);
+// console.log(typeof newS1);
+// console.log(newS2);
+// console.log(typeof newS2);
+
+
+// //null
+// let s = null;
+
+// let newS1 = Number(s);
+// let newS2 = String(s);
+// let newS3 = Boolean(s);
+
+
+// console.log(newS1);
+// console.log(typeof newS1);
+// console.log(newS2);
+// console.log(typeof newS2);
+// console.log(newS3);
+// console.log(typeof newS3);
+
+
+// //undefined
+// let u;
+
+// let newU1 = Number(u);
+// let newU2 = String(u);
+// let newU3 = Boolean(u);
+
+// console.log(newU1); // for u =""; = 0
+// console.log(typeof newU1);
+// console.log(newU2);
+// console.log(typeof newU2);
+// console.log(newU3);
+// console.log(typeof newU3);
+
+
+// //arithmetic ops
+// console.log(10+20);
+// console.log(10-20);
+// console.log(10*20);
+// console.log(10/20);
+// console.log(10**20);
+// console.log(10%20);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
+console.log(1 + 2 + "2" + 3);
+console.log(1 + 2 + "2" + 3 - 3);
+console.log(1 + 2 + "2" + 3 * 3);
+console.log(1 - 2 - "2" - 3 - 3);
+
