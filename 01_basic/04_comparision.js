455a7f00488b4faf053863114a144e5a825f1f55
saipraneeth9396 <praneeth9396@gmail.com>
2025-10-29 11:00:12 +0000

    comparision ops

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,41 @@
+// //comparision ops
+// console.log(1 == 2);
+// console.log(1 != 2);
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <=2);
+
+
+// //
+// console.log("1" > 2);
+// console.log("1" < 2);
+// console.log(1 > "2");
+// console.log(1 < "2");
+
+
+// // 
+// console.log(null > 0);
+// console.log(null >= 0); // null treated as 0
+// console.log(null == 0); // == is different from other comp ops // == wont convert null to number
+
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+
+
+// //
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+
+
+// === -> strict check i.e. no conversion
+console.log("2" === 2);
+
+
+
